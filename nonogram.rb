01698b82b60b3afe11a40320d9043b4c40a9d2a3
BuildTools <gmbouvier@gmail.com>
2016-08-17 16:51:39 -0700

    Refactored and working better

--- nonogram.rb
+++ nonogram.rb
@@ -2,220 +2,344 @@
 require 'tk'
 require './parse_puzzle.rb'
 
-puzzle = parse_puzzle('15776.xml')
-
 class Block
-	attr_reader(:coords, :colour, :active)
-	attr_accessor(:tkrec)
+  attr_reader(:coords, :color)
+  attr_accessor(:selected)
 
-	def initialize(canvas, x, y)
-		@coords = {x: x, y: y}
-		@colour = :white
-    @active = false
-
-    @canvas = canvas
-		@tkrec = nil
-    @marked_x = []
-	end
-
-  def colour_swap
-    if @colour == :white
-      @colour = :black
-    elsif @colour == :black
-      @colour = :white
-    elsif @colour == :mark
-      @colour = :white
-    end
-    update_view()
+  # Init a Block
+  def initialize(x, y, color)
+    @coords = {x: x, y: y}
+    @color = color
+    @selected = false
   end
 
-  def toggle_x
-    if colour == :mark
-      colour_swap
+  # Change the colour
+  def color_cycle(colors)
+    if @color != :mark
+      @color = colors[colors.find_index(@color)-1]
     else
-      @colour = :mark
-      update_view()
+      @color = colors[0]
     end
   end
 
-  def draw_self(scale)
-    x1 = @coords[:x]*scale
-    y1 = @coords[:y]*scale
-    @tkrec = TkcRectangle.new(@canvas, x1, y1, x1+scale, y1+scale)
-    @tkrec.fill(@colour.to_s).outline('#aaaaaa')
-    @tkrec.bind("ButtonPress-1", proc {colour_swap()} )
-    @tkrec.bind("ButtonPress-3", proc {toggle_x()})
-  end
-
-  def update_view
-    if @colour == :mark
-      @tkrec.fill('white')
-      @marked_x << TkcLine.new(@canvas, @tkrec.coords[0], @tkrec.coords[1], @tkrec.coords[2], @tkrec.coords[3], { 'width' => 2 })
-      @marked_x << TkcLine.new(@canvas, @tkrec.coords[2], @tkrec.coords[1], @tkrec.coords[0], @tkrec.coords[3], { 'width' => 2 })
-      @marked_x.map { |line| line.bind("ButtonPress-3", proc {colour_swap()}) }
-      @marked_x.map { |line| line.bind("ButtonPress-1", proc {colour_swap()}) }
-    else
-      if !@marked_x.empty?
-        @marked_x.each { |line| line.remove }
-        @marked_x = []
-      end
-      @tkrec.fill(@colour.to_s)
+  # If not marked, mark it
+  def set_marked
+    if @color != :mark
+      @color = :mark
     end
   end
 
-end
-
-
-def draw_clues(puzzle, left_edge, top_edge, scale)
-  puzzle["rows"].each_with_index do |row, r|
-    row.reverse.each_with_index do |clue, i|
-      TkcText.create(left_edge,
-                     left_edge.width-(((i+1)*scale)-(scale/2)),
-                     ((r+1)*scale)-(scale/2),
-                     {'text' => clue})
-    end
-  end
-  puzzle["columns"].each_with_index do |column, c|
-    column.reverse.each_with_index do |clue, i|
-      TkcText.create(top_edge,
-                     (((c+1)*scale)-(scale/2)),
-                     top_edge.height-(((i+1)*scale)-(scale/2)),
-                     {'text' => clue})
-    end
-  end
 end
 
 class Board
+  attr_reader(:width, :height, :clues, :colors, :blocks)
 
-  def initialize(root, puzzle, scale)
-    @width = puzzle["columns"].length
-    @height = puzzle["rows"].length
+  # Init the Board
+  def initialize(puzzle)
+    @width = puzzle[:columns].length
+    @height = puzzle[:rows].length
 
-    @scale = 20
-    @widthpx = @width*scale
-    @heightpx = @height*scale
+    @clues = puzzle.reject { |key,_| ![:rows, :columns].include? key }
+    @colors = [:white, :black]
 
-    @canvas = TkCanvas.new(root,
-                          bg: "#ffffff",
-                          height: @heightpx,
-                          width: @widthpx) { grid(:row => 1, :column => 1)}
-
+    # Init the blocks
     @blocks = blocks_init()
-    @active_block = @blocks.find {|b| b.coords == {x: 0, y: 0}}
-    @active_lines = []
-
-    @canvas.bind('Motion', proc {|x, y| draw_active(block_from_pos(x, y))}, "%x %y")
-    @canvas.bind('Leave', proc {remove_active()})
-
-    root.bind('l', proc do
-      draw_active(@blocks.find do |b|
-        b.coords == {x: @active_block.coords[:x]+1, y: @active_block.coords[:y]}
-      end)
-    end)
-    root.bind('h', proc do
-      draw_active(@blocks.find do |b|
-        b.coords == {x: @active_block.coords[:x]-1, y: @active_block.coords[:y]}
-      end)
-    end)
-    root.bind('k', proc do
-      draw_active(@blocks.find do |b|
-        b.coords == {x: @active_block.coords[:x], y: @active_block.coords[:y]-1}
-      end)
-    end)
-    root.bind('j', proc do
-      draw_active(@blocks.find do |b|
-        b.coords == {x: @active_block.coords[:x], y: @active_block.coords[:y]+1}
-      end)
-    end)
-    root.bind('c', proc { @active_block.colour_swap })
-    root.bind('x', proc { @active_block.toggle_x })
-
-    draw_blocks()
-    draw_guides()
-    draw_active(@active_block)
   end
 
   def blocks_init
     blocks = []
     (0...@width).each do |x|
       (0...@height).each do |y|
-        blocks << Block.new(@canvas, x, y)
+        blocks << Block.new(x, y, @colors[0])
       end
+    end
+    blocks[0].selected = true
+    blocks
+  end
+
+end
+
+class GameWindow
+
+  def initialize(root, board, scale)
+    @root = root
+    set_root_keybinds()
+    @scale = scale
+    @board = board
+    @board_window = draw_board_window()
+    set_board_window_binds()
+    @clues_windows = draw_clues_windows(@board.clues)
+    @clues = draw_clues(@board.clues)
+    @blocks = draw_blocks()
+    @guide_lines = draw_guide_lines()
+    @marks = {}
+    @highlights = []
+  end
+
+  def set_root_keybinds
+    # Zoom in and out
+    @root.bind('plus', proc do
+      @scale += 5
+      update_scale()
+    end)
+    @root.bind('minus', proc do
+      @scale -= 5
+      update_scale()
+    end)
+    # HJKL Vim Keybinds
+    @root.bind('h', proc do
+      active = @board.blocks.find { |b| b.selected }
+      move_to = @board.blocks.find { |b| b.coords == {x: active.coords[:x]-1, y: active.coords[:y]} }
+      if move_to
+        active.selected = false
+        move_to.selected = true
+        update_block_view(move_to)
+      end
+    end)
+    @root.bind('j', proc do
+      active = @board.blocks.find { |b| b.selected }
+      move_to = @board.blocks.find { |b| b.coords == {x: active.coords[:x], y: active.coords[:y]+1} }
+      if move_to
+        active.selected = false
+        move_to.selected = true
+        update_block_view(move_to)
+      end
+    end)
+    @root.bind('k', proc do
+      active = @board.blocks.find { |b| b.selected }
+      move_to = @board.blocks.find { |b| b.coords == {x: active.coords[:x], y: active.coords[:y]-1} }
+      if move_to
+        active.selected = false
+        move_to.selected = true
+        update_block_view(move_to)
+      end
+    end)
+    @root.bind('l', proc do
+      active = @board.blocks.find { |b| b.selected }
+      move_to = @board.blocks.find { |b| b.coords == {x: active.coords[:x]+1, y: active.coords[:y]} }
+      if move_to
+        active.selected = false
+        move_to.selected = true
+        update_block_view(move_to)
+      end
+    end)
+    @root.bind('c', proc do
+      active = @board.blocks.find { |b| b.selected }
+      active.color_cycle(@board.colors)
+      update_block_view(active)
+    end)
+    @root.bind('x', proc do
+      active = @board.blocks.find { |b| b.selected }
+      active.set_marked()
+      update_block_view(active)
+    end)
+  end
+
+  def set_board_window_binds
+    def floor_to(number, step)
+      whole, _ = number.divmod(step)
+      whole * step
+    end
+
+    @board_window.bind('Motion', proc do |x, y|
+      x = floor_to(x, @scale)/@scale
+      y = floor_to(y, @scale)/@scale
+      block = @board.blocks.find {|b| b.coords == {x: x, y: y}}
+      if block
+        @board.blocks.select {|b| b.selected == true}.each do |b|
+          b.selected = false
+          update_block_view(b)
+        end
+        block.selected = true
+        update_block_view(block)
+      end
+    end, "%x %y")
+    @board_window.bind('Leave', proc { erase_highlight() })
+  end
+
+  # Init the main canvas
+  def draw_board_window
+    heightpx = @board.height*@scale
+    widthpx = @board.width*@scale
+    TkCanvas.new(@root, bg: "#ffffff", height: heightpx, width: widthpx) { grid(row: 1, column: 1) }
+  end
+
+  # Init the clues canvas'
+  def draw_clues_windows(clues)
+    heightpx = @board.height*@scale
+    widthpx = @board.width*@scale
+
+    windows = {}
+    windows[:rows] = TkCanvas.new(@root, bg: "#aaaaaa") { grid(:row => 1, :column => 0) }
+    windows[:rows].height = heightpx
+    windows[:rows].width = (@scale*clues[:rows].map { |row| row.length }.max)
+    windows[:columns] = TkCanvas.new(@root, bg: "#aaaaaa") { grid(:row => 0, :column => 1) }
+    windows[:columns].height = (@scale*clues[:columns].map { |column| column.length }.max)
+    windows[:columns].width = widthpx
+    windows
+  end
+
+  # Draw the clues
+  def draw_clues(clues)
+    clue_list = []
+    rows = @clues_windows[:rows]
+    columns = @clues_windows[:columns]
+    clues[:rows].each_with_index do |row, r|
+      row.reverse.each_with_index do |clue, i|
+        clue_list << TkcText.new(rows, rows.width-(((i+1)*@scale)-(@scale/2)),
+                                ((r+1)*@scale)-(@scale/2),
+                                { text: clue })
+      end
+    end
+    clues[:columns].each_with_index do |column, c|
+      column.reverse.each_with_index do |clue, i|
+        clue_list << TkcText.new(columns, ((c+1)*@scale)-(@scale/2),
+                                columns.height-(((i+1)*@scale)-(@scale/2)),
+                                { text: clue })
+      end
+    end
+  clue_list
+  end
+
+  # Draw the blocks on the board
+  def draw_blocks
+    blocks = {}
+    @board.blocks.each do |block|
+      x = block.coords[:x]*@scale
+      y = block.coords[:y]*@scale
+      cell = TkcRectangle.new(@board_window, x, y, x+@scale, y+@scale, { fill: block.color.to_s, outline: "#aaaaaa" })
+
+      # Bind events
+      cell.bind("ButtonPress-1", proc do
+        block.color_cycle(@board.colors)
+        update_block_view(block)
+      end)
+      cell.bind("ButtonPress-3", proc do
+        if block.color != :mark
+          block.set_marked()
+        else
+          block.color_cycle(@board.colors)
+        end
+        update_block_view(block)
+      end)
+
+      blocks[block] = cell
     end
     blocks
   end
 
-  def floor_to(number, step)
-    whole, _ = number.divmod(step)
-    whole * step
-  end
-
-  def block_from_pos(x, y)
-    x = floor_to(x, @scale)/@scale
-    y = floor_to(y, @scale)/@scale
-    @blocks.find {|b| b.coords == {x: x, y: y}}
-  end
-
-  def draw_blocks
-    @blocks.each { |block| block.draw_self(@scale) }
-  end
-
-  def draw_guides
-    (0..@canvas.width).step(@scale*5) do |x|
-      TkcLine.new(@canvas, x, 0, x, @canvas.height, {'width' => '2', 'fill' => '#aaaaaa'})
+  # Draw guide lines every 5 blocks
+  def draw_guide_lines
+    lines = []
+    (0...@board_window.width).step(@scale*5) do |x|
+      lines << TkcLine.new(@board_window, x, 0, x, @board_window.height, { width: 2, fill: "#aaaaaa" })
     end
-    (0..@canvas.height).step(@scale*5) do |x|
-      TkcLine.new(@canvas, 0, x, @canvas.width, x, {'width' => '2', 'fill' => '#aaaaaa'})
+    (0...@board_window.height).step(@scale*5) do |x|
+      lines << TkcLine.new(@board_window, 0, x, @board_window.width, x, { width: 2, fill: "#aaaaaa" })
     end
+    lines
   end
 
-  def draw_active(block)
-    if block != @active_block
-      if block
-        @active_block = block
-        block_coords = block.tkrec.coords
-        @active_lines.each {|line| line.remove}
-        @active_lines = []
-        @active_lines << TkcLine.new(@canvas, block_coords[0], 0, block_coords[0], @canvas.height, {'width' => 1, 'fill' => 'red'})
-        @active_lines << TkcLine.new(@canvas, block_coords[2], 0, block_coords[2], @canvas.height, {'width' => 1, 'fill' => 'red'})
-        @active_lines << TkcLine.new(@canvas, 0, block_coords[1], @canvas.width, block_coords[1], {'width' => 1, 'fill' => 'red'})
-        @active_lines << TkcLine.new(@canvas, 0, block_coords[3], @canvas.width, block_coords[3], {'width' => 1, 'fill' => 'red'})
-      end
+  def draw_highlight(cell)
+    erase_highlight()
+    @highlights = []
+    @highlights << TkcLine.new(@board_window, cell.coords[0], 0, cell.coords[0], @board_window.height, { fill: 'red', width: 2 })
+    @highlights << TkcLine.new(@board_window, cell.coords[2], 0, cell.coords[2], @board_window.height, { fill: 'red', width: 2 })
+    @highlights << TkcLine.new(@board_window, 0, cell.coords[1], @board_window.height, cell.coords[1], { fill: 'red', width: 2 })
+    @highlights << TkcLine.new(@board_window, 0, cell.coords[3], @board_window.width, cell.coords[3], { fill: 'red', width: 2 })
+  end
+
+  def erase_highlight
+    unless @highlights.empty?
+      @highlights.each {|highlight| highlight.remove}
     end
   end
 
-  def remove_active
-    @active_lines.each {|line| line.remove}
-    @active_lines = []
+  # Update the block visual
+  def update_block_view(block)
+    cell = @blocks[block]
+    delete_mark(block)
+    if block.color != :mark
+      cell.fill(block.color.to_s)
+    else
+      cell.fill("white")
+      mark = []
+      mark << TkcLine.new(@board_window, cell.coords[0], cell.coords[1], cell.coords[2], cell.coords[3], { 'width' => 2 })
+      mark << TkcLine.new(@board_window, cell.coords[2], cell.coords[1], cell.coords[0], cell.coords[3], { 'width' => 2 })
+      mark.map { |line| line.bind("ButtonPress-1", proc do
+        block.color_cycle(@board.colors)
+        update_block_view(block)
+      end) }
+      mark.map { |line| line.bind("ButtonPress-3", proc do
+        block.color_cycle(@board.colors)
+        update_block_view(block)
+      end) }
+      # Save the lines used for marking so they can be removed
+      @marks[block] = mark
+    end
+    if block.selected
+      draw_highlight(cell)
+    end
+  end
+
+  # Given a block, if it is marked, remove the mark
+  def delete_mark(block)
+    if @marks[block]
+      @marks[block].each { |line| line.remove }
+      @marks.delete(block)
+    end
+  end
+
+  # Update the window to reflect a change in scale
+  def update_scale
+    heightpx = @board.height*@scale
+    widthpx = @board.width*@scale
+    # Gameboard
+    @board_window.height = heightpx
+    @board_window.width = widthpx
+    # Clue windows
+    @clues_windows[:rows].height = heightpx
+    @clues_windows[:rows].width = (@scale*@board.clues[:rows].map { |row| row.length }.max)
+    @clues_windows[:columns].height = (@scale*@board.clues[:columns].map { |column| column.length }.max)
+    @clues_windows[:columns].width = widthpx
+    @clues.each { |clue| clue.delete() }
+    @clues = draw_clues(@board.clues)
+    # Blocks
+    @blocks.each do |block, cell|
+      x = block.coords[:x]*@scale
+      y = block.coords[:y]*@scale
+      cell.coords = [x, y, x+@scale, y+@scale]
+      update_block_view(block)
+    end
+    # Guide lines
+    @guide_lines.each { |line| line.remove }
+    @guide_lines = draw_guide_lines()
   end
 
 end
 
+# A main funtion for getting things going
 def main(puzzle)
 
-  width = puzzle["columns"].length
-  height = puzzle["rows"].length
+  puzzle = parse_puzzle(puzzle)
   scale = 20
-  widthpx = width*scale
-  heightpx = height*scale
 
-	root = TkRoot.new(bg: "#222222") { title "Nonogram" }
+  # Check to see if we were passed a valid puzzle
+  if puzzle && puzzle[:colors] == 2
 
-  left_edge = TkCanvas.new(root,
-                           bg: "#aaaaaa",
-                           height: heightpx,
-                           width: (scale*5)) { grid(:row => 1, :column => 0)}
-  top_edge = TkCanvas.new(root,
-                          bg: "#aaaaaa",
-                          height: (scale*5),
-                          width: widthpx) { grid(:row => 0, :column => 1)}
+    # Build everything
+    root = TkRoot.new(bg: "#222222") { title "Nonogram" }
+    board = Board.new(puzzle)
+    gamewindow = GameWindow.new(root, board, scale)
 
-  board = Board.new(root, puzzle, scale)
+    # Start the loop
+    Tk.mainloop
 
-  draw_clues(puzzle, left_edge, top_edge, scale)
-
-	Tk.mainloop
+  else
+    puts "Please provide a valid 2 colour puzzle"
+  end
 
 end
 
-main(puzzle)
+# Start the game!
+main(ARGV[0])
--- parse_puzzle.rb
+++ parse_puzzle.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/env ruby
+#!/usr/bin/env ruby-2.1
 require 'xmlsimple'
 
 def parse_puzzle(xml)
@@ -9,18 +9,34 @@ def parse_puzzle(xml)
 
   puzzle_xml["puzzle"][0]["clues"][0]["line"].each do |line|
     counts = []
-    line["count"].each { |x| counts << x.to_i }
+    line["count"].each do |x|
+      if x.class == Hash
+        counts << x["content"].to_i
+      else
+        counts << x.to_i
+      end
+    end unless !line["count"]
     first << counts
   end
 
   puzzle_xml["puzzle"][0]["clues"][1]["line"].each do |line|
     counts = []
-    line["count"].each { |x| counts << x.to_i }
+    line["count"].each do |x|
+      if x.class == Hash
+        counts << x["content"].to_i
+      else
+        counts << x.to_i
+      end
+    end unless !line["count"]
     second << counts
   end
 
-  { puzzle_xml["puzzle"][0]["clues"][0]["type"].to_s => first,
-    puzzle_xml["puzzle"][0]["clues"][1]["type"].to_s => second}
+  { puzzle_xml["puzzle"][0]["clues"][0]["type"].to_sym => first,
+    puzzle_xml["puzzle"][0]["clues"][1]["type"].to_sym => second,
+    :metadata => { :title => puzzle_xml["puzzle"][0]["title"][0],
+                   :author => puzzle_xml["puzzle"][0]["author"][0],
+                   :copyright => puzzle_xml["puzzle"][0]["copyright"][0],
+                   :id => puzzle_xml["puzzle"][0]["id"][0],
+                   :description => puzzle_xml["puzzle"][0]["description"][0] },
+    :colors => puzzle_xml["puzzle"][0]["color"].length}
 end
-
-puzzle = parse_puzzle('webpbn004495.xml')
